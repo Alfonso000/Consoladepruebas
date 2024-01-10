a387037b1411b035383a9d9a2e335e1e94827e61
Alfonso Sanchis <9105895@alumnos.ufv.es>
2024-01-10 16:30:05 +0100

    subir el main que antes no

--- src/com.example.helloworld/Perritos.java
+++ src/com.example.helloworld/Perritos.java
@@ -1,2 +1,14 @@
-package com.example.helloworld;public class Perritos {
+package com.example.helloworld;
+
+public class Perritos {
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    private String raza;
+
 }
